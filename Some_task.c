3e5f317402a4c0dd43c126c7c374db32bf4fb768
Kaguya_Hime <DarthEminenz@gmail.com>
2017-03-11 02:30:35 +0300

    Добавлены комментарии

--- Some_task.c
+++ Some_task.c
@@ -8,8 +8,8 @@ const char*months[] = { "January","February","March","April","May","June","July"
 "1","2","3","4","5","6","7", "8", "9", "10", "11", "12", 
 "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december", 
 "Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec", 
-"jan","feb","mar","apr","may","june","july","aug","sept","oct","nov","dec" }; 
-
+"jan","feb","mar","apr","may","june","july","aug","sept","oct","nov","dec" }; //Массив с месяцами, записанными в различной форме
+Я
 main() 
 { 
 	time_t timer; 	
@@ -38,21 +38,21 @@ main()
 	} 
 	if (value == 0) 
 	{ 
-		printf("Wrong input data\n"); 
+		printf("Wrong input data\n"); //проверка на правильность ввода
 	} 
 	printf("Enter year\n"); 
 	int year = 0; 
 	scanf("%d", &year); 
 	if (year > 2017 || year < 1900) 
 	{ 
-		printf("Wrong input data\n"); 
+		printf("Wrong input data\n"); //проверка на правильность ввода
 	} 
 	printf("Enter day\n"); 
 	int day = 0; 
 	scanf("%d", &day); 
 	if (day > 31 || day<1) 
 	{ 
-		printf("Wrong input data\n"); 
+		printf("Wrong input data\n"); //проверка на правильность ввода
 	} 
 	date.tm_mon = value-1; 
 	date.tm_year = year; 
